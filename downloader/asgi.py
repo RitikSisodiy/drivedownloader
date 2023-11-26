d86a087f0ee0ade13004ca6d38e025ac715a2584
RitikSisodiy <59529659+RitikSisodiy@users.noreply.github.com>
2023-11-27 03:18:00 +0530

    Update asgi.py

--- downloader/asgi.py
+++ downloader/asgi.py
@@ -1,16 +1,28 @@
-"""
-ASGI config for cflax project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
 
 import os
-import django
-from channels.routing import get_default_application
+
+from django.core.asgi import get_asgi_application
+
+# ******************** #
+import downloader.routing
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+# ******************** #
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'downloader.settings')
-django.setup()
-application = get_default_application()
+
+# application = get_asgi_application() # This must be replaced
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            downloader.routing.websocket_urlpatterns
+        )
+    )
+})
