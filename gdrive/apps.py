df1d3b8119ae011435eea63a830f970e1739f8d1
FittDev1 <developemfitt@gmail.com>
2023-11-26 17:27:55 +0530

    added  call to keep service up

--- gdrive/apps.py
+++ gdrive/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+import threading,time
+import requests
+def callService():
+    url = "https://drivedownloader.onrender.com/login/"
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+    }
+    requests.get(url,headers=headers)
+    time.sleep(20)
+    callService()
 
 class GdriveConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gdrive'
+    def ready(self):
+        thread = threading.Thread(target=callService, args=())
+        thread.start()
