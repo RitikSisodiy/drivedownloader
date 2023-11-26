587406b24dd2d34318bcbc6f0aabd714ac52171d
RitikSisodiy <59529659+RitikSisodiy@users.noreply.github.com>
2023-11-27 02:28:00 +0530

    Update settings.py

--- downloader/settings.py
+++ downloader/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from .settings_config import configuration
+import gevent.monkey
+gevent.monkey.patch_all()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
