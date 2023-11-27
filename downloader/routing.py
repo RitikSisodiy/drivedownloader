1032d84a960f7f3397cb97d723e6923bafcd12e5
ritik <ritik.nimoy@gemseducation.com>
2023-11-27 11:20:52 +0530

    fix deployement issue

--- downloader/routing.py
+++ downloader/routing.py
@@ -1,12 +1,7 @@
 
 from gdrive import consumers
-from channels.routing import ProtocolTypeRouter,URLRouter
 from django.urls import path
-from channels.auth import AuthMiddlewareStack    
-application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-          path("ws/downloads/",consumers.downloadClassConsumer.as_asgi())
-    ])
-    )
-})
+  
+websocket_urlpatterns = [
+        path("ws/downloads/",consumers.downloadClassConsumer.as_asgi())
+]
