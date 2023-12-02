1b85565208103ef453cd0006bbdf7f2387645d27
ritik <ritik.nimoy@gemseducation.com>
2023-12-02 16:23:03 +0530

    added model in  admin

--- gdrive/admin.py
+++ gdrive/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import driveModel
+from .models import driveModel,downloadHistory,Downloading
 
 admin.site.register(driveModel)
+admin.site.register(Downloading)
+admin.site.register(downloadHistory)
